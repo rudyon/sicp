b87ea428d3d4d57dbe66ef0bdef97790175a3d58
rudyon <keremberket@gmail.com>
2023-06-08 11:10:41 +0300

    strting from the beginning because i wasn't satisfied

--- exercises/exercise1.3.rkt
+++
@@ -1,8 +0,0 @@
-#lang sicp
-(define (square x) (* x x))
-(define (sum-of-squares x y)
-  (+ (square x) (square y)))
-(define (sum-of-two-larger-squares x y z)
-  (cond ((and (<= y x) (<= y z)) (sum-of-squares x z))
-	((and (<= x y) (<= x z)) (sum-of-squares y z))
-	((and (<= z x) (<= z y)) (sum-of-squares x y))))
